df6f64532b7433f6bfb1aab87bcbe50496b8eadc
ShoroveTajmen <sorovitajmen@gmail.com>
2019-09-22 22:32:05 +0600

    aaaa

---
+++ 16092019/program_1.py
@@ -0,0 +1,8 @@
+name = "shorove tajmen"
+age = 19
+cgpa = 3.92
+
+print(name + "is a new student in our class")
+print("she is" , age , "years old")
+print(name + " has  scored", cgpa , "in her secondary exam")
+print("aaa")
