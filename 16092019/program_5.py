4d89947775738d0259adc37598309426df447c81
ShoroveTajmen <sorovitajmen@gmail.com>
2019-09-23 23:30:26 +0600

    type_casting

---
+++ 16092019/program_5.py
@@ -0,0 +1,11 @@
+n1 = int(input("Enter first number:"))
+n2 = int(input("Enter second number:"))
+
+result = n1 + n2
+print("sum is",result)
+
+result = n1 - n2
+print("difference is",result)
+
+result = n1 * n2
+print("mulplication is",result)
